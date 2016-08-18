e951bfcc6cbd994fbbbd15bb69287ccf630af9b8
Seamus Conacher <seamusconacher@gmail.com>
2016-08-18 17:12:45 +0100

    Added GUI and stereo volume controls

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -62,7 +62,7 @@ public:
     {
     public:
         MainWindow (String name)  : DocumentWindow (name,
-                                                    Colours::lightgrey,
+                                                    Colours::lightslategrey,
                                                     DocumentWindow::allButtons)
         {
             setUsingNativeTitleBar (true);
--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,11 +1,3 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #ifndef MAINCOMPONENT_H_INCLUDED
 #define MAINCOMPONENT_H_INCLUDED
 
@@ -22,7 +14,19 @@ public:
 	//==============================================================================
 	MainContentComponent()
 	{
-		setSize(800, 600);
+		levelSliderL.setRange(0.0, 0.25);
+		levelSliderL.setTextBoxStyle(Slider::TextBoxRight, false, 100, 20);
+		levelSliderR.setRange(0.0, 0.25);
+		levelSliderR.setTextBoxStyle(Slider::TextBoxRight, false, 100, 20);
+		levelLabelL.setText("Noise Level L", dontSendNotification);
+		levelLabelR.setText("Noise Level R", dontSendNotification);
+
+		addAndMakeVisible(levelSliderL);
+		addAndMakeVisible(levelSliderR);
+		addAndMakeVisible(levelLabelL);
+		addAndMakeVisible(levelLabelR);
+
+		setSize(600, 75);
 
 		// No inputs, two outputs
 		setAudioChannels(0, 2);
@@ -46,15 +50,31 @@ public:
 
 	void getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill) override
 	{
+		const float levelL = (float)levelSliderL.getValue();
+		const float levelR = (float)levelSliderR.getValue();
+		float level;
+
 		for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
 		{
+			if (channel == 0)
+			{
+				level = levelL;
+			}
+			else
+			{
+				level = levelR;
+			}
+
 			// Get a pointer to the start sample in the buffer for this audio output channel
 			float* const buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
 
 			// Fill the required number of samples with noise between -0.125 and 0.125
 			for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
 			{
-				buffer[sample] = random.nextFloat() * 0.25f - 0.125f;
+				const float noise = random.nextFloat() * 2.0f - 1.0f;
+				// While this method of volume control will add distortion as the slider is moved,
+				// the output is noise anyway, so in this situation it is fine
+				buffer[sample] = noise * level;
 			}
 		}
 	}
@@ -64,22 +84,20 @@ public:
 		Logger::getCurrentLogger()->writeToLog("Releasing audio resources");
 	}
 
-	//==============================================================================
-	void paint(Graphics& g) override
-	{
-		// (Our component is opaque, so we must completely fill the background with a solid colour)
-		g.fillAll(Colours::black);
-	}
-
 	void resized() override
 	{
-		// This is called when the MainContentComponent is resized.
-		// If you add any child components, this is where you should
-		// update their positions.
+		levelLabelL.setBounds(10, 10, 90, 20);
+		levelLabelR.setBounds(10, 40, 92, 20);
+		levelSliderL.setBounds(100, 10, getWidth() - 110, 20);
+		levelSliderR.setBounds(100, 40, getWidth() - 110, 20);
 	}
 
 
 private:
+	Slider levelSliderL;
+	Slider levelSliderR;
+	Label levelLabelL;
+	Label levelLabelR;
 	Random random;
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainContentComponent)
 };
